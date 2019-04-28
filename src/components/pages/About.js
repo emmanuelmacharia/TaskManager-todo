b060fd1d43bf88edf41a600d2bee9397949bbe95
emmanuelmacharia <samuelmarsha@outlook.com>
2019-04-28 21:20:59 +0300

    addded more in the aboutus

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <React.Fragment>
         <h1>About</h1>
-        <p>This is a todo application</p>
+        <p>This is a todo application that enables you to keep track of not only the mot important tasks, but also the mundane ones. Tasker enables you to be more productive every day by giving you a simple and straight-forward way to manage what needs to be done, when and how.</p>
     </React.Fragment>
   )
 }
